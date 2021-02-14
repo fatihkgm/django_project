01ecd7685919f2121d5f10f601a811152ee5050e
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-02-14 13:15:10 -0500

    database update1

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 #created tittle in a field of Post database
@@ -8,4 +9,8 @@ class Post(models.Model):
     tittle = models.CharField(max_length=100)
     content = models.TextField()
     data_posted = models.DateField(default = timezone.now)
+    #one user can multiple post but only one author for one post
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
 
