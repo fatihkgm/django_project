c1965ccc17599ec0ffb860a19cd709c2dc551f4b
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-02-14 14:33:42 -0500

    database update4

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,17 @@
 from django.shortcuts import render
-from models
+from .models import Post
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})
+    return render(request, 'blog/about.html', {'tittle': 'About'})
+
+
